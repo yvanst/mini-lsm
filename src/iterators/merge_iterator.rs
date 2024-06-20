733143dcee96c11ac67dbff25f7fd0a678b0b017
Yifei <yvanstu@gmail.com>
2024-06-20 09:34:13 +0800

    day 2 update

--- src/iterators/merge_iterator.rs
+++ src/iterators/merge_iterator.rs
@@ -1,8 +1,8 @@
 #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
-use std::borrow::BorrowMut;
 use std::cmp::{self};
+use std::collections::binary_heap::PeekMut;
 use std::collections::BinaryHeap;
 
 use anyhow::Result;
@@ -84,37 +84,83 @@ impl<I: 'static + for<'a> StorageIterator<KeyType<'a> = KeySlice<'a>>> StorageIt
     }
 
     fn is_valid(&self) -> bool {
-        !(self.iters.is_empty() & self.current.is_none())
+        // !(self.iters.is_empty() & self.current.is_none())
+        self.current
+            .as_ref()
+            .map(|x| x.1.is_valid())
+            .unwrap_or(false)
     }
 
     fn next(&mut self) -> Result<()> {
-        dbg!(0);
-        match &mut self.current {
-            Some(current) => {
-                while !self.iters.is_empty() {
-                    let mut heap_min = self.iters.pop().unwrap();
-                    if heap_min.1.key() > current.1.key() {
-                        self.iters.push(heap_min);
-                        break;
-                    } else if heap_min.1.is_valid() {
-                        let _ = heap_min.borrow_mut().1.next();
-                        dbg!(heap_min.1.key(), heap_min.1.value());
-                        self.iters.push(heap_min);
-                    }
+        //     dbg!(0);
+        //     match &mut self.current {
+        //         Some(current) => {
+        //             while !self.iters.is_empty() {
+        //                 let mut heap_min = self.iters.pop().unwrap();
+        //                 if heap_min.1.key() > current.1.key() {
+        //                     self.iters.push(heap_min);
+        //                     break;
+        //                 } else if heap_min.1.is_valid() {
+        //                     let _ = heap_min.borrow_mut().1.next();
+        //                     dbg!(heap_min.1.key(), heap_min.1.value());
+        //                     self.iters.push(heap_min);
+        //                 }
+        //             }
+        //         }
+        //         None => (),
+        //     }
+        //     dbg!(1);
+        //     if self.current.is_some() {
+        //         let mut current = self.current.take().unwrap();
+        //         if current.1.is_valid() {
+        //             let _ = current.1.next();
+        //             self.iters.push(current);
+        //         }
+        //     }
+        //     self.current = self.iters.pop();
+        //     dbg!(self.current.as_ref().map(|s| s.1.key()));
+        //     Ok(())
+        // }
+        let current = self.current.as_mut().unwrap();
+        // Pop the item out of the heap if they have the same value.
+        while let Some(mut inner_iter) = self.iters.peek_mut() {
+            debug_assert!(
+                inner_iter.1.key() >= current.1.key(),
+                "heap invariant violated"
+            );
+            if inner_iter.1.key() == current.1.key() {
+                // Case 1: an error occurred when calling `next`.
+                if let e @ Err(_) = inner_iter.1.next() {
+                    PeekMut::pop(inner_iter);
+                    return e;
                 }
-            }
-            None => (),
-        }
-        dbg!(1);
-        if self.current.is_some() {
-            let mut current = self.current.take().unwrap();
-            if current.1.is_valid() {
-                let _ = current.1.next();
-                self.iters.push(current);
+
+                // Case 2: iter is no longer valid.
+                if !inner_iter.1.is_valid() {
+                    PeekMut::pop(inner_iter);
+                }
+            } else {
+                break;
             }
         }
-        self.current = self.iters.pop();
-        dbg!(self.current.as_ref().map(|s| s.1.key()));
+
+        current.1.next()?;
+
+        // If the current iterator is invalid, pop it out of the heap and select the next one.
+        if !current.1.is_valid() {
+            if let Some(iter) = self.iters.pop() {
+                *current = iter;
+            }
+            return Ok(());
+        }
+
+        // Otherwise, compare with heap top and swap if necessary.
+        if let Some(mut inner_iter) = self.iters.peek_mut() {
+            if *current < *inner_iter {
+                std::mem::swap(&mut *inner_iter, current);
+            }
+        }
+
         Ok(())
     }
 }
--- src/lsm_iterator.rs
+++ src/lsm_iterator.rs
@@ -17,7 +17,15 @@ pub struct LsmIterator {
 
 impl LsmIterator {
     pub(crate) fn new(iter: LsmIteratorInner) -> Result<Self> {
-        Ok(Self { inner: iter })
+        let mut iter = Self { inner: iter };
+        let _ = iter.move_to_non_delete();
+        Ok(iter)
+    }
+    fn move_to_non_delete(&mut self) -> Result<()> {
+        while self.is_valid() && self.value().is_empty() {
+            self.inner.next()?
+        }
+        Ok(())
     }
 }
 
@@ -37,12 +45,9 @@ impl StorageIterator for LsmIterator {
     }
 
     fn next(&mut self) -> Result<()> {
-        let mut res = self.inner.next();
-        // skip deleted key
-        while self.value().is_empty() {
-            res = self.inner.next();
-        }
-        res
+        self.inner.next()?;
+        self.move_to_non_delete()?;
+        Ok(())
     }
 }
 
@@ -75,10 +80,16 @@ impl<I: StorageIterator> StorageIterator for FusedIterator<I> {
     }
 
     fn key(&self) -> Self::KeyType<'_> {
+        if self.has_errored || !self.iter.is_valid() {
+            panic!("invalid access to the underlying iterator")
+        }
         self.iter.key()
     }
 
     fn value(&self) -> &[u8] {
+        if self.has_errored || !self.iter.is_valid() {
+            panic!("invalid access to the underlying iterator")
+        }
         self.iter.value()
     }
 
@@ -86,15 +97,12 @@ impl<I: StorageIterator> StorageIterator for FusedIterator<I> {
         if self.has_errored {
             bail!("Error occurred in the Iterator");
         }
-        if !self.is_valid() {
-            bail!("No Iterators");
-        }
-        match self.iter.next() {
-            ok @ Ok(_) => ok,
-            err @ Err(_) => {
+        if self.is_valid() {
+            if let Err(e) = self.iter.next() {
                 self.has_errored = true;
-                err
+                return Err(e);
             }
         }
+        Ok(())
     }
 }
